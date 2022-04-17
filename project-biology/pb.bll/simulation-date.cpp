eaba026328e1cb31f55f5f5523aa6111dd82a54d
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2022-04-17 21:35:33 +0300

    Fix simulation date timer

--- project-biology/pb.bll/simulation-date.cpp
+++ project-biology/pb.bll/simulation-date.cpp
@@ -1,132 +1,40 @@
 #include "pch.h"
 #include "simulation-date.h"
 
-int getIndex(std::string month)
-{
-	std::vector<std::string> months = {
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December"
-	};
-
-	std::vector<std::string>::iterator it;
-	it = std::find(months.begin(), months.end(), month);
-
-	return std::distance(months.begin(), it);
-}
-
 void switchDate(int& day, std::string& month)
 {
-	int monthNumber = getIndex(month);
+    std::vector<std::string> months = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    };
+    std::vector<int> days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	switch (monthNumber)
-	{
-	case 0: 
-		if (day == 31)
-		{
-			day = 0;
-			month = "February";
-		}
-		day++;
-		break;
-	case 1: 
-		if (day == 28)
-		{
-			day = 0;
-			month = "March";
-		}
-		day++;
-		break;
-	case 2:
-		if (day == 31)
-		{
-			day = 0;
-			month = "April";
-		}
-		day++;
-		break;
-	case 3:
-		if (day == 30)
-		{
-			day = 0;
-			month = "May";
-		}
-		day++;
-		break;
-	case 4:
-		if (day == 31)
-		{
-			day = 0;
-			month = "June";
-		}
-		day++;
-		break;
-	case 5:
-		if (day == 30)
-		{
-			day = 0;
-			month = "July";
-		}
-		day++;
-		break;
-	case 6:
-		if (day == 31)
-		{
-			day = 0;
-			month = "August";
-		}
-		day++;
-		break;
-	case 7:
-		if (day == 31)
-		{
-			day = 0;
-			month = "September";
-		}
-		day++;
-		break;
-	case 8:
-		if (day == 30)
-		{
-			day = 0;
-			month = "October";
-		}
-		day++;
-		break;
-	case 9:
-		if (day == 31)
-		{
-			day = 0;
-			month = "November";
-		}
-		day++;
-		break;
-	case 10:
-		if (day == 30)
-		{
-			day = 0;
-			month = "December";
-		}
-		day++;
-		break;
-	case 11:
-		if (day == 31)
-		{
-			day = 0;
-			month = "January";
-		}
-		day++;
-		break;
-	}
+    int i = 0;
+    while (i < 12)
+    {
+        if (days[i] == day && months[i] == month)
+        {
+            day = 0;
+            if (i == 11)
+            {
+                month = months[0];
+                break;
+            }
+            month = months[i + 1];
+        }
+        i++;
+    }
+    day++;
 }
 
 void timer(int& day, std::string& month)
--- project-biology/pb.bll/simulation-date.h
+++ project-biology/pb.bll/simulation-date.h
@@ -2,7 +2,6 @@
 
 #include "pch.h"
 
-int getIndex(std::string);
 void switchDate(int&, std::string&);
 
 void timer(int&, std::string&);
